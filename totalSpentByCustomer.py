a547cf22d23131fae9ceddcb6b446a7a11ddbc17
Seonggu Lee <gf265@naver.com>
2021-05-25 18:56:27 +0900

    고객 별 총 지출 비용 계산

---
+++ totalSpentByCustomer.py
@@ -0,0 +1,25 @@
+from ast import parse
+from pyspark import SparkContext, SparkConf
+
+
+def parseLine(line):
+    fields = line.split(',')
+    userID = int(fields[0])
+    cost = float(fields[2])
+    return (userID, cost)
+
+
+if __name__ == "__main__":
+    conf = SparkConf().setAppName("helloWorld").setMaster("local")
+    sc = SparkContext(conf=conf)
+
+    lines = sc.textFile("data/customer-orders.csv")
+
+    userCosts = lines.map(parseLine)
+
+    totalSpentByCustomer = userCosts.reduceByKey(lambda x, y: x + y)
+
+    results = totalSpentByCustomer.collect()
+
+    for userID, total in results:
+        print(f'{userID} : {total:0.2f}$')
